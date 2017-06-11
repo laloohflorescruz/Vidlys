d29417ae706bdf0f2bc89b4c614ce5dad7dd7310
Lalo Flores Cruz <eflores@crudcreativo.com>
2017-06-11 10:07:38 -0400

    Firts Commit
    Customer and Movie models

---
+++ Vidlys/Controllers/MoviesController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidlys.Models;
+using Vidlys.ViewModels;
+
+namespace Vidlys.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
+
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate (int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customer 2" }
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+        //}
+    }
+}
---
+++ Vidlys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidlys.Startup))]
+namespace Vidlys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
