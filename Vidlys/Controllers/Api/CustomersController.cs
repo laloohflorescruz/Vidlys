6732b9049f0ec047144c3996f8662c8d00927307
Lalo Flores Cruz <eflores@crudcreativo.com>
2017-06-18 22:16:30 -0400

    WebApi camelCase configuration. CustomerController AutoMapper

--- Vidlys/Controllers/Api/CustomersController.cs
+++ Vidlys/Controllers/Api/CustomersController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Vidlys.DTO;
 using Vidlys.Models;
 
 namespace Vidlys.Controllers.Api
@@ -16,34 +18,37 @@ namespace Vidlys.Controllers.Api
         {
             _context = new ApplicationDbContext();
         }
-        public IEnumerable<Customer> GetCustomers()
+        public IEnumerable<CustomerDTO> GetCustomers()
         {
-            return _context.Customers.ToList();
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDTO>);
         }
 
-        public Customer GetCustomer(int id)
+        public CustomerDTO GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            return customer;
+            return Mapper.Map<Customer, CustomerDTO>(customer);
         }
         [HttpPost]
-        public Customer CreateCustomer(Customer customer)
+        public CustomerDTO CreateCustomer(CustomerDTO customerDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
+            var customer = Mapper.Map<CustomerDTO, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
-            return customer;
+            customerDto.Id = customer.Id;
+
+            return customerDto;
 
         }
         [HttpPut]
-        public void UpdateCustomer(int id, Customer customer)
+        public void UpdateCustomer(int id, CustomerDTO customerDto)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
@@ -53,14 +58,17 @@ namespace Vidlys.Controllers.Api
             if (customerInDb == null)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
 
-            customerInDb.Name = customer.Name;
-            customerInDb.Birthday = customer.Birthday;
-            customerInDb.IsSuscribedToNewsLetter = customer.IsSuscribedToNewsLetter;
-            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            Mapper.Map(customerDto, customerInDb);
+
+            //customerInDb.Name = customer.Name;
+            //customerInDb.Birthday = customer.Birthday;
+            //customerInDb.IsSuscribedToNewsLetter = customer.IsSuscribedToNewsLetter;
+            //customerInDb.MembershipTypeId = customer.MembershipTypeId;
 
             _context.SaveChanges();
 
         }
+
         public void DeleteCustomer(int id)
         {
             var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
